1c6a6c079d023c64fee25dc29636b8077e649dfe
ashoksainiengineer <120373064+ashoksainiengineer@users.noreply.github.com>
2025-12-19 11:28:46 +0530

    done

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Font Awesome aur Industrial Fonts load karne ke liye */}
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
       </head>
       <body className="antialiased overflow-hidden">
